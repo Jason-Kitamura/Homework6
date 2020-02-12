c3a2df41f4d3f965ae350e43787f2b04db3fc263
JaceKace <jasonkitamura@gmail.com>
2020-02-11 21:04:31 -0500

    Updated weather icons, running 5day function, still needs error message if user submits NaCity

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById('search-btn').addEventListener('click', createCity)
-        document.getElementById('search-btn').addEventListener('click', retreiveData)
+        // document.getElementById('search-btn').addEventListener('click', retreiveData)
         document.getElementById('cityList').addEventListener('click', retreiveDataFromList)
 
        let newCity
@@ -52,6 +52,7 @@ document.getElementById('search-btn').addEventListener('click', createCity)
 
         function createCity () {
 
+
             newCity = $('#cityInput').val();
             
             console.log('search button');
@@ -64,6 +65,8 @@ document.getElementById('search-btn').addEventListener('click', createCity)
             cityArray.push( newCity );
 
             localStorage.cityArray = JSON.stringify( cityArray );
+
+            retreiveData();
         };
 
         function retreiveData(){
@@ -99,9 +102,16 @@ document.getElementById('search-btn').addEventListener('click', createCity)
             console.log(response);
             console.log(response.name);
             $('#cityName').html(response.name);
+            $('#description').html(`Weather: ${response.weather[0].description}`);
             $('#temperature').html(`Temperature: ${Math.round(response.main.temp-273.15)}°C`);
             $('#humidity').html(`Humidity: ${response.main.humidity}%`);
             $('#windSpeed').html(`Wind Speed: ${response.wind.speed} mph`);
+
+            var iconCode = response.weather[0].icon;
+            var iconurl = `https://openweathermap.org/img/w/${iconCode}.png`;
+
+            $(`#weatherIcon`).attr('src', iconurl);
+
             // $('#uvIndex').html(`UV Index: ${response.name}`);
         }
 
@@ -109,22 +119,25 @@ document.getElementById('search-btn').addEventListener('click', createCity)
 
             $('#dayList').html('');
 
-            console.log(response2.list[7].main.temp_max);
 
             for(i=7; i<40; i+=8) {
             var date = response2.list[i].dt_txt;
             date = date.substring(0,11);
 
+            var iconCode = response2.list[i].weather[0].icon;
+            var iconurl = `https://openweathermap.org/img/w/${iconCode}.png`;
+
             $('#dayList').append(`
                 <div class=" weatherDay">
                     <div id="innerWeatherDay"
                         <p>${date}</p>
-                        <p>${response2.list[i].weather.id}</p>
+                        <div id="icon"><img id="${i}" src="" alt="Weather icon"></div>
                         <p>Temp: ${Math.round(response2.list[i].main.temp-273.15)}°</p>
                         <p>Humidity: ${response2.list[i].main.humidity}</p>
                     </div>
                 </div>
             `);
+            $(`#${i}`).attr('src', iconurl);
             }
         }
         
